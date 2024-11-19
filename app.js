104bb7c4d0e7b71542acd532463f002b597ab71b
mbalbino1997 <rmassimo.balbino@gmail.com>
2024-11-19 19:03:44 +0100

    add crud

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+const posts = require("./data/posts.js");
 app.use(express.static("public"));
 
+app.get("/posts", (req, res) => {
+    res.send("lista dei post")
+});
+
+app.get("/posts/:id", (req, res) => {
+    res.send(`Dettagli del post ${req.params.id}`)
+});
+
+app.post("/posts", (req, res) => {
+    res.send("Creazione nuovo post")
+});
+
+app.put("/posts/:id", (res, req) => {
+    res.send("Modifica totale del post")
+})
 
 
 
 
-
+const server = app.listen(port, () => {
+    console.log(`server in ascolto sula porta ${port}`)
+})
 process.on('SIGINT', () => {
     server.close();
     console.log('Exit Server');
